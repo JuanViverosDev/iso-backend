bb839ede5a38383a49aaf6683228e6563f6d55b9
Juan Viveros <jviverosd1@gmail.com>
2024-03-18 17:46:47 -0500

    Init

---
+++ Iso.Backend.Application/Base/RepositoryBase.cs
@@ -0,0 +1,113 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using Iso.Backend.Application.Common.Interfaces;
+using Iso.Backend.Domain.Entities.Base;
+
+namespace Iso.Backend.Application.Base
+{
+    public abstract class RepositoryBase<TDomain, TContext> : IRepository<TDomain> where TDomain : DomainObject where TContext : DbContext
+    {
+        protected readonly TContext _db;
+
+        protected RepositoryBase(TContext db)
+        {
+            _db = db;
+        }
+
+        public virtual async Task<TDomain> AddAsync(TDomain entity)
+        {
+            await _db.AddAsync(entity);
+            await _db.SaveChangesAsync();
+            return entity;
+        }
+
+        public virtual async Task<TDomain> FindAsync(
+             Guid id,
+             params Expression<Func<TDomain, object>>[] includes)
+        {
+            IQueryable<TDomain> query = _db.Set<TDomain>();
+            if (includes.Any())
+            {
+                foreach (var include in includes)
+                {
+                    query = query.Include(include);
+                }
+            }
+            return await query
+                    .Where(u => !u.IsDeleted && u.Id == id)
+                    .SingleOrDefaultAsync().ConfigureAwait(false);
+        }
+
+        public virtual async Task<IEnumerable<TDomain>> FindAsync(
+            Expression<Func<TDomain, bool>> predicate,
+            params Expression<Func<TDomain, object>>[] includes)
+        {
+            IQueryable<TDomain> query = _db.Set<TDomain>();
+            if (includes.Any())
+            {
+                foreach (var include in includes)
+                {
+                    query = query.Include(include);
+                }
+            }
+            return await query
+                .Where(predicate)
+                .Where(u => !u.IsDeleted)
+                .Where(u => u.IsActive)
+                .ToListAsync()
+                .ConfigureAwait(false);
+        }
+
+        public virtual async Task<TDomain> FindOneAsync(
+            Expression<Func<TDomain, bool>> predicate,
+            params Expression<Func<TDomain, object>>[] includes)
+        {
+            IQueryable<TDomain> query = _db.Set<TDomain>();
+            if (includes.Any())
+            {
+                foreach (var include in includes)
+                {
+                    query = query.Include(include);
+                }
+            }
+            return await query
+                .Where(predicate)
+                .Where(u => !u.IsDeleted)
+                .Where(u => u.IsActive)
+                .SingleOrDefaultAsync()
+                .ConfigureAwait(false);
+        }
+
+        public virtual async Task DeleteAsync(TDomain entity)
+        {
+            _db.Remove(entity);
+            await _db.SaveChangesAsync()
+                     .ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// Remove - Soft Delete
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public virtual async Task RemoveAsync(TDomain entity)
+        {
+            entity.IsDeleted = true;
+            entity.Modified = DateTime.UtcNow;
+            _db.Update(entity);
+            await _db
+                .SaveChangesAsync()
+                .ConfigureAwait(false);
+        }
+
+        public virtual async Task UpdateAsync(TDomain entity)
+        {
+            entity.Modified = DateTime.UtcNow;
+            _db.Update(entity);
+            await _db
+                .SaveChangesAsync()
+                .ConfigureAwait(false);
+        }
+    }
+}
---
+++ Iso.Backend.Application/Common/Interfaces/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+
+namespace Iso.Backend.Application.Common.Interfaces
+{
+    public interface IRepository<T>
+    {
+        Task<T> AddAsync(T entity);
+
+        Task DeleteAsync(T entity);
+
+        Task<T> FindAsync(Guid id, params Expression<Func<T, object>>[] includes);
+
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includes);
+
+        Task<T> FindOneAsync(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includes);
+
+        Task UpdateAsync(T entity);
+
+        Task RemoveAsync(T entity);
+    }
+}
